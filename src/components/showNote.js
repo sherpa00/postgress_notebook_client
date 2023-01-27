61065a86127014f165e36a7894e4e0adaabdb0fe
Chhewang Sherpa <87354583+sherpa00@users.noreply.github.com>
2023-01-27 22:15:29 +0545

    Added update note functionality

--- src/components/showNote.js
+++ src/components/showNote.js
@@ -45,8 +45,12 @@ function ShowNotes() {
 
 function SingleNote({note}) {
 
+    const [isUpdateActive,setIsUpdateActive] = useState(false);
+    const [newTitle,setNewTitle] = useState(note.title);
+    const [newNote,setNewNote] = useState(note.note)
+
     const hanldeDeleteNote = async (e) => {
-        let id = e.target.id;
+        let id = note.noteid;
         let deleteData = await fetch(`https://postgres-notebook.onrender.com/api/note/${id}`,{
             method: "DELETE",
         });
@@ -58,22 +62,75 @@ function SingleNote({note}) {
         }
     }
 
+    const handleNewTitle = (e) => {
+        setNewTitle(e.target.value);
+    }
+
+    const handleNewNote = (e) => {
+        setNewNote(e.target.value);
+    }
+
+    const handleUpdateSubmit = async (e) => {
+        try {
+            e.preventDefault();
+            let id = note.noteid;
+            let resObj = {
+                title: newTitle,
+                note: newNote
+            };
+            let updateData = await fetch(`https://postgres-notebook.onrender.com/api/note/${id}`,{
+                method: "PUT",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(resObj)
+            });
+            if (updateData.ok) {
+                let updateDataJson = await updateData.json();
+                console.log(updateDataJson);
+                setIsUpdateActive(false);
+            } else {
+                console.log("Error while updating the note");
+            }
+        } catch (err) {
+            console.log(err);
+            console.log("Error while updating notes");
+        }
+    }
+
+    if (!isUpdateActive) {
+        return (
+            <div className="single-note">
+                                                <h2 className="title">
+                                                    {
+                                                        note.title
+                                                    }
+                                                </h2>
+                                                <p className="note">
+                                                    {
+                                                        note.note
+                                                    }
+                                                </p>
+                                                <div>
+                                                    <button className="update" id={note.noteid} onClick={() => setIsUpdateActive(true)}>Update</button>
+                                                    <button className="delete" id={note.noteid} onClick={hanldeDeleteNote}>Delete</button>
+                                                </div>
+                                            </div>
+        )
+    }
+
     return (
         <div className="single-note">
-            <h2 className="title">
-                {
-                    note.title
-                }
-            </h2>
-            <p className="note">
-                {
-                    note.note
-                }
-            </p>
-            <div>
-                <button className="update" id={note.noteid}>Update</button>
-                <button className="delete" id={note.noteid} onClick={hanldeDeleteNote}>Delete</button>
-            </div>
+            <form onSubmit={handleUpdateSubmit}>
+                <label htmlFor="newTitle">New Title</label>
+                <input type="text" id="newTitle" name="newTitle" placeholder="enter new Title" value={newTitle} onChange={handleNewTitle} required/>
+
+                <label htmlFor="newNote">New Note</label>
+                <textarea type="text" id="newNote" name="newNote" value={newNote} onChange={handleNewNote}  required ></textarea>
+
+                <button type="submit" className="update" id={note.noteid}>Update</button>
+                <button onClick={() => setIsUpdateActive(false)} className="cancel">Cancel</button>
+            </form>
         </div>
     )
 }
